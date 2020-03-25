bb91072a21db9f4fbceb3d9d0cb9303bf211331b
gsimim17 <56830009+gsimim17@users.noreply.github.com>
2020-03-25 20:28:58 +0100

    Create Main.java

---
+++ GschierJava01/src/k4/k4_5/k4_5_2/Main.java
@@ -0,0 +1,60 @@
+import k4.ArrayBsp02;
+import k4.MehrdimensionalesArrayBsp01;
+import k4.MehrdimensionalesArrayBsp02;
+import k4.ByteKitBsp02;
+import k5.AusdrueckeBsp01;
+import k5.StringVerkettungBsp01;
+import k5.ReferenzGleichheitBsp01;
+import k5.OperatorVorrangregelnBsp01;
+import k6.GelabeltesBreakBsp01;
+
+// Listing 4.11
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Listing 4.8: ");
+        System.out.println("(ArrayBsp02:) ");
+        ArrayBsp02 bsp1 = new ArrayBsp02();
+        bsp1.arrayBsp02();
+        
+        System.out.println("\nListing 4.9: ");
+        System.out.println("(MehrdimensionalesArrayBsp01:) ");
+        MehrdimensionalesArrayBsp01 bsp2 = new MehrdimensionalesArrayBsp01();
+        bsp2.mehrdimensionalesArrayBsp01();
+        
+        System.out.println("\nListing 4.10: ");
+        System.out.println("(MehrdimensionalesArrayBsp02:) ");
+        MehrdimensionalesArrayBsp02 bsp3 = new MehrdimensionalesArrayBsp02();
+        bsp3.mehrdimensionalesArrayBsp02();
+        
+        System.out.println("\nListing 4.12 - 4.13: ");
+        System.out.println("(ByteKitBsp01, ByteKitBsp02:) ");
+        ByteKitBsp02 bsp9 = new ByteKitBsp02();
+        bsp9.byteKitBsp02();
+        
+        System.out.println("\nListing 5.1 - 5.2: ");
+        System.out.println("(AusdrueckeBsp01:) ");
+        AusdrueckeBsp01 bsp4 = new AusdrueckeBsp01();
+        bsp4.ausdrueckeBsp01();
+        
+        System.out.println("\nListing 5.3 - 5.5: ");
+        System.out.println("(StringVerkettungBsp01:) ");
+        StringVerkettungBsp01 bsp5 = new StringVerkettungBsp01();
+        bsp5.stringVerkettungBsp01();
+        
+        System.out.println("\nListing 5.6 - 5.7: ");
+        System.out.println("(ReferenzGleichheitBsp01:) ");
+        ReferenzGleichheitBsp01 bsp6 = new ReferenzGleichheitBsp01();
+        bsp6.referenzGleichheitBsp01();
+        
+        System.out.println("\nListing 5.8 - 5.9: ");
+        System.out.println("(OperatorVorrangregelnBsp01:) ");
+        OperatorVorrangregelnBsp01 bsp7 = new OperatorVorrangregelnBsp01();
+        bsp7.operatorVorrangregelnBsp01();
+        
+        System.out.println("\nListing 6.6: ");
+        System.out.println("(GelabeltesBreakBsp01:) ");
+        GelabeltesBreakBsp01 bsp8 = new GelabeltesBreakBsp01();
+        bsp8.gelabeltesBreakBsp01();
+    }
+}
