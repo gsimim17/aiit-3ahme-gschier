e3fcb808271cb4baf525b7498230e77573a21fab
gsimim17 <56830009+gsimim17@users.noreply.github.com>
2020-04-20 14:23:23 +0200

    Create Object.java

---
+++ GschierJava04/src/ue02_serienschaltung/Object.java
@@ -0,0 +1,9 @@
+package ue02_serienschaltung;
+
+public class Object {
+    String toString() {
+        protected java.lang.Object clone() throws CloneNotSupportedException {
+            return super.clone(); //To change body of generated methods, choose Tools | Templates.
+        }
+    }
+}
