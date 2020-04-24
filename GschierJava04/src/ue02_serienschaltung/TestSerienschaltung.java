7203420587cf2e8134ffa6d9c0d27c55f32114dd
gsimim17 <56830009+gsimim17@users.noreply.github.com>
2020-04-24 21:12:51 +0200

    Update TestSerienschaltung.java

--- GschierJava04/src/ue02_serienschaltung/TestSerienschaltung.java
+++ GschierJava04/src/ue02_serienschaltung/TestSerienschaltung.java
@@ -3,16 +3,22 @@ package ue02_serienschaltung;
 public class TestSerienschaltung {
     public static void main(String[] args) {
         final Serienschaltung serienschaltung = new Serienschaltung();
-        System.out.println("1: " + serienschaltung);
+        System.out.print("1: ");
+        serienschaltung.serienschaltung();
         serienschaltung.addWiderstand(10);
-        System.out.println("2: " + serienschaltung);
+        System.out.print("2: ");
+        serienschaltung.serienschaltung();
         serienschaltung.setStrom(0.5);
-        System.out.println("3: " + serienschaltung);
+        System.out.print("3: ");
+        serienschaltung.serienschaltung();
         serienschaltung.addWiderstand(15);
-        System.out.println("4: " + serienschaltung);
+        System.out.print("4: ");
+        serienschaltung.serienschaltung();
         serienschaltung.addWiderstand(20);
-        System.out.println("5: " + serienschaltung);
+        System.out.print("5: ");
+        serienschaltung.serienschaltung();
         serienschaltung.setStrom(1);
-        System.out.println("6: " + serienschaltung);
+        System.out.print("6: ");
+        serienschaltung.serienschaltung();
     }
 }
