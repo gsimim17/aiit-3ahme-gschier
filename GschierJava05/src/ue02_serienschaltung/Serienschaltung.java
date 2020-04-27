845e3fc80595f575a954744b4e00953d1192d656
gsimim17 <56830009+gsimim17@users.noreply.github.com>
2020-04-27 13:28:22 +0200

    Create Serienschaltung.java

---
+++ GschierJava05/src/ue02_serienschaltung/Serienschaltung.java
@@ -0,0 +1,58 @@
+package ue02_serienschaltung;
+
+public class Serienschaltung {
+    double strom = 0;
+    double spannung = 0;
+    double[] widerstandsFeld = new double[100];
+    int widerstandsFeldLength = widerstandsFeld.length;
+    
+    double updateSpannung() {
+        double gesamtspannung = 0;
+        
+        for (int i = 0; i < widerstandsFeldLength; i++) {
+            gesamtspannung += strom * widerstandsFeld[i];
+        }
+        
+        return gesamtspannung;
+    }
+    
+    void addWiderstand(double widerstandInOhm) {
+        if (widerstandInOhm < 0 || widerstandInOhm > 10) {
+            
+        }
+        
+        widerstandsFeldLength++;
+        widerstandsFeld[widerstandsFeldLength-1] = widerstandInOhm;
+        spannung = updateSpannung();
+    }
+    
+    /*
+    double getStrom() {
+        
+    }
+    */
+    
+    void setStrom(double strom) {
+        this.strom = strom;
+        spannung = updateSpannung();
+    }
+    
+    /*
+    double getSpannung() {
+        
+    }
+    */
+    
+    /*
+    String toString() {
+        protected Object clone() throws CloneNotSupportedException {
+            return super.clone(); //To change body of generated methods, choose Tools | Templates.
+        }
+    }
+    */
+
+    void serienschaltung() {
+        int arrayLength = widerstandsFeld.length;
+        System.out.print("Serienschaltung{strom=" + strom + ", spannung=" + spannung + ", widerstandsFeld=" + arrayLength + "}\n");
+    }
+}
